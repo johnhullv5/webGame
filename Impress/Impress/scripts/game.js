8630f32af663696abb7e51dad771344f8518fe54
johnhullv5 <jhihll@163.com>
2016-06-06 16:54:55 -0400

    Add files via upload

--- Impress/Impress/scripts/game.js
+++ Impress/Impress/scripts/game.js
@@ -1,2 +1,6 @@
 /// <reference path="typings/impress/impress.d.ts" />
+function init() {
+    console.log("check its running or not?...");
+    impress().init();
+}
 //# sourceMappingURL=game.js.map
--- Impress/Impress/scripts/game.ts
+++ Impress/Impress/scripts/game.ts
@@ -1,1 +1,6 @@
 ﻿/// <reference path="typings/impress/impress.d.ts" />
+function init()
+{
+    console.log("check its running or not?...")
+    impress().init();
+}
